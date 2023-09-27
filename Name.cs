8c2a96d5b11c458f70c991febe9eec5a7e21ccbe
Giorgi Menteshashvili <93538315+Gmentesh2@users.noreply.github.com>
2023-09-28 00:00:42 +0400

    Delete Name.cs

--- Name.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CountriesAPI
-{
-    public class Name
-    {
-        public string? Common { get; set; }
-        public string? Official { get; set; }
-        public Dictionary<string, NativeName>? NativeName { get; set; }
-    }
-}
