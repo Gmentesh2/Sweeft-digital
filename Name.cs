c712aed28ae3280185fe62ed2ac86c2137b139d6
Giorgi Menteshashvili <93538315+Gmentesh2@users.noreply.github.com>
2023-09-27 23:58:24 +0400

    Add files via upload

---
+++ Name.cs
@@ -0,0 +1,9 @@
+﻿namespace CountriesAPI
+{
+    public class Name
+    {
+        public string? Common { get; set; }
+        public string? Official { get; set; }
+        public Dictionary<string, NativeName>? NativeName { get; set; }
+    }
+}
